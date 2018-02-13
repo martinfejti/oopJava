a9825f1a77f190cb8b920351b5ec0c811051fcb9
fejti <fejti@iit431.iit.uni-miskolc.hu>
2018-02-13 17:52:06 +0100

    second one

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HelloWorld {
+	
+	public static void main (String args[]) {
+	
+		System.out.println("Zsömle");
+		
+		int a = 10;
+		System.out.println(a*a);
+	
+	}
+}
