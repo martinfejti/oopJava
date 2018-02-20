87da1024f84fbf25d55bb2ab6142bd11ab00673f
fejti <fejti@iit431.iit.uni-miskolc.hu>
2018-02-20 17:35:23 +0100

    1_2 projekt

---
+++ 1_2/src/bumm/Bumm.java
@@ -0,0 +1,18 @@
+package bumm;
+
+public class Bumm {
+
+	public static void main(String[] args) {
+		
+		int i;
+		
+		for(i=10;i>10;i--) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Bumm");
+		
+		
+	}
+
+}
